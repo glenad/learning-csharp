326f674fff87e2ab8eadbf7019b35051d2c4547e
Glen Adams <glen.adams@directaxis.co.za>
2017-01-10 17:55:04 +0200

    Add DelegateWriter

---
+++ PluralSight/CSharp5Fundamentals/DelegateWriter/DelegateWriter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+            Writer writer = new Writer(logger.WriteMessage);
+            
+            writer("Success");
+        }
+    }
+
+    public delegate void Writer(string message);
+
+    public class Logger
+    {
+        public void WriteMessage(String message)
+        {
+            Console.WriteLine(message);
+            Console.ReadLine();
+            
+        }
+    }
+}
